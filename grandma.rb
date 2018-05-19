9340c3fb45f5a190f9b5cf6212736eeca2784540
CHRISTOPHER WOOD <cjwood032@gmail.com>
2018-05-19 15:12:48 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak to grandma(greeting)
-  if greeting==greeting.upcase ? return "NO, NOT SINCE 1938!" : return "HUH?! SPEAK UP, SONNY!"
+  if greeting==greeting.upcase ? "NO, NOT SINCE 1938!" : "HUH?! SPEAK UP, SONNY!"
     
   end
 end
